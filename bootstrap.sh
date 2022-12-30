842cbaf886ef78097ed8cc2d6c2f23940f299d77
Franck Mazzolo <franck.mazzolo@gmail.com>
2022-12-30 10:51:41 +0100

    feat: brew bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env zsh
+
+#${BASH_SOURCE[0]} equivalent in zsh is ${(%):-%N}
+SOURCE="(%):-%x"
+
+cd "$(dirname "${SOURCE}")";
+
+git pull origin master;
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude "bootstrap.sh" \
+		-avh --no-perms . ~;
+	source ~/.zprofile;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
